14650eac214b77c12486d7c9a236d62e15f3d967
Nimesh Katariya <nimesh.katariya23@gmail.com>
2026-03-05 08:42:04 +0530

    Add team players to show teams

--- app/Policies/TeamPolicy.php
+++ app/Policies/TeamPolicy.php
@@ -20,7 +20,8 @@ class TeamPolicy
      */
     public function view(User $user, Team $team): bool
     {
-        return $user->id === $team->created_by;
+        return $user->id === $team->created_by
+            || $team->allPlayers()->where('user_id', $user->id)->exists();
     }
 
     /**
